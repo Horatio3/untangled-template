6cb6d2e944400e121dc190e885224ef2b3dca45d
Horatio3 <gamingmonkey2287@gmail.com>
2019-08-05 11:23:48 +0100

    movement

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -30,34 +30,44 @@ function draw() {
   if (second() % 3 == 1 && colourswitch == false) {
     colourswitch = true
   }
-  
+
   if (second() % 3 == 0 && colourswitch) {
 
-  col.r = random(0, 255)
-  col.g = random(0, 255)
-  col.b = random(0, 255)
-  colourswitch = false 
-  } 
+    col.r = random(0, 255)
+    col.g = random(0, 255)
+    col.b = random(0, 255)
+    colourswitch = false
+  }
 
   //movement
   if (elipseX > 1270) {
-    bounce = true}
-  else if (elipseX< 0) {bounce = false}
+    bounce = true
+  } else if (elipseX < 0) {
+    bounce = false
+  }
 
-  
-  if (bounce ==  true) { elipseX = elipseX -  3}
-  if (bounce == false) { elipseX = elipseX + 3}
-  if (bounce ==  true) { elipseY  = elipseY - 3}
-  if (bounce == false) { elipseY= elipseY + 3}
-  
+
+  if (bounce == true) {
+    elipseX = elipseX - 3
+    elipseY = elipseY - 3
+  }
+  if (bounce == false) {
+    elipseX = elipseX + 3
+
+    elipseY = elipseY + 3
+  }
+
+
+
+
   // colour the background white
   background(255);
   // select white as a colour
-  fill (col.r,col.g,col.b);
+  fill(col.r, col.g, col.b);
   // draw a rectangle
-  rect(0, 150, 100, 100);
+  rect(elipseX, elipseY, 100, 100);
   //colour of circle
-  fill(col.r,col.g,col.b)
+  fill(col.r, col.g, col.b)
   //making the circle
-  ellipse(elipseX, elipseY, 100, 100)
+  ellipse(elipseX, 100, 100, 100)
 }
