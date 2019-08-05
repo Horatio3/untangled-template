766587812c63cc301486a0e6670298e6267ffc06
Horatio3 <gamingmonkey2287@gmail.com>
2019-08-05 10:28:07 +0100

    rect fill changed

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -7,7 +7,7 @@ function draw() {
   // colour the background black
   background(0);
   // select white as a colour
-  fill(255);
+  fill (100);
   // draw a rectangle
   rect(150, 150, 100, 100);
 }
